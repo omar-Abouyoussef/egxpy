b1147a63b9573d027f061a33dfdb637feb9984e1
Omar <101734132+omar-Abouyoussef@users.noreply.github.com>
2025-04-16 23:18:35 +0200

    Update Download_Data.py

--- Download_Data.py
+++ Download_Data.py
@@ -19,9 +19,16 @@ def to_excel(df):
 st.set_page_config(page_title="Download Data", layout='wide')
 
 
-st.markdown("""
-<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-6NW0KJWXVF"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-6NW0KJWXVF'); </script>""",
-            unsafe_allow_html=True)
+st.markdown("""<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6NW0KJWXVF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6NW0KJWXVF');
+</script>""", unsafe_allow_html=True)
+
 st.markdown("""
     <style>
     /* Import Google Font */
